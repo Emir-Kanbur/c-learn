d8580b6378e8cb50b6db10bd4a78d546479c11c8
Sopwit <131982697+Sopwit@users.noreply.github.com>
2026-09-18 11:52:54 +0300

    operatorler

---
+++ operator-1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int sayi1, sayi2;
+
+  sayi1 = 10 + 33;
+
+  sayi2 = sayi1 - 6;
+
+  printf("Sayi1 nedir: %d ", sayi1);
+
+  printf("\n");
+
+  printf("Sayi2 nedir: %d", sayi2);
+
+  return 0;
+}
